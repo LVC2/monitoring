87e3084ede47a8ff1d05044a3fcfc528b3564c48
Vasl <lvc2vas@gmail.com>
2026-09-11 16:22:28 +0500

    feat: model real APACS card access events

--- ApacsMonitor/Models/EventRecord.cs
+++ ApacsMonitor/Models/EventRecord.cs
@@ -4,10 +4,13 @@ public sealed class EventRecord
 {
     public DateTime RealTime { get; init; }
     public DateTime RegisterTime { get; init; }
-    public string InitObjectName { get; init; } = "";
+    public string FullName { get; init; } = "";
+    public string CardNumber { get; init; } = "";
+    public string Location { get; init; } = "";
+    public string Direction { get; init; } = "";
+    public string ReaderName { get; init; } = "";
+    public string RawObjectName { get; init; } = "";
     public int EventType { get; init; }
-    public int InitObjectId0 { get; init; }
-    public int InitObjectId1 { get; init; }
     public int SekId0 { get; init; }
     public int SekId1 { get; init; }
 }
