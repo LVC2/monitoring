2002d2f2ae472f0cfb544e1d04381c6d3c1395d2
Vasl <lvc2vas@gmail.com>
2026-09-14 10:14:33 +0500

    feat: prepare EventRecord for asynchronous APACS photos

--- ApacsMonitor/Models/EventRecord.cs
+++ ApacsMonitor/Models/EventRecord.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace ApacsMonitor.Models;
 
-public sealed class EventRecord
+public sealed class EventRecord : INotifyPropertyChanged
 {
+    private byte[]? _photoBytes;
+
     public DateTime RealTime { get; init; }
     public DateTime RegisterTime { get; init; }
+    public string LastName { get; init; } = "";
+    public string FirstName { get; init; } = "";
+    public string MiddleName { get; init; } = "";
     public string FullName { get; init; } = "";
     public string CardNumber { get; init; } = "";
     public string Location { get; init; } = "";
@@ -11,8 +19,26 @@ public sealed class EventRecord
     public string ReaderName { get; init; } = "";
     public string RawObjectName { get; init; } = "";
     public int HolderId { get; init; }
-    public byte[]? PhotoBytes { get; set; }
+
+    public byte[]? PhotoBytes
+    {
+        get => _photoBytes;
+        set
+        {
+            if (ReferenceEquals(_photoBytes, value))
+                return;
+
+            _photoBytes = value;
+            OnPropertyChanged();
+        }
+    }
+
     public int EventType { get; init; }
     public int SekId0 { get; init; }
     public int SekId1 { get; init; }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    private void OnPropertyChanged([CallerMemberName] string? propertyName = null) =>
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 }
