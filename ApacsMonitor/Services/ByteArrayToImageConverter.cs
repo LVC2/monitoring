f7b035dd5bc30885a42ae03d175bc3af0dc3f30b
Vasl <lvc2vas@gmail.com>
2026-09-14 08:09:51 +0500

    feat: add byte array image converter

---
+++ ApacsMonitor/Services/ByteArrayToImageConverter.cs
@@ -0,0 +1,37 @@
+using System.Globalization;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ApacsMonitor.Services;
+
+public sealed class ByteArrayToImageConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not byte[] bytes || bytes.Length == 0)
+            return null!;
+
+        try
+        {
+            using var stream = new MemoryStream(bytes, writable: false);
+            var image = new BitmapImage();
+            image.BeginInit();
+            image.CacheOption = BitmapCacheOption.OnLoad;
+            image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+            image.StreamSource = stream;
+            image.EndInit();
+            image.Freeze();
+            return image;
+        }
+        catch
+        {
+            return null!;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotSupportedException();
+    }
+}
